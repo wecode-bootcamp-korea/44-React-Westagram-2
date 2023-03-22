a49f5424d34f509c0fd2e8a4d3ecf3d73d2d9dd9
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-23 08:54:22 +0900

    ADD : 신효민 첫 commit 현재 진행 상황 - 이미지 파일옮기기 및 컴포넌트 분리 중. - Login 화면 수정 중 - Main 댓글삭제 기능 구현 완료

--- src/pages/hyominShin/Login/LoginShin.js
+++ src/pages/hyominShin/Login/LoginShin.js
@@ -1,7 +1,34 @@
-import React from 'react';
+/* eslint-disabled  */
+import React, { useState } from 'react';
+import { Link, navigate, Route, useNavigate } from 'react-router-dom';
+import Main from '../Main/MainShin';
+import '../../../styles/common.scss';
+import './LoginShin.scss';
 
-function LoginShin() {
-  return <div></div>;
-}
+const Login = () => {
+  return (
+    <div className="body">
+      <form className="loginWindow">
+        <p className="westaLogo">westagram</p>
+        <input
+          className="idInput"
+          type={'text'}
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={e => {}}
+        />
+        <input
+          className="passwordInput"
+          type={'password'}
+          placeholder="비밀번호"
+          onChange={e => {}}
+        />
+        <button className="loginButton" disabled>
+          로그인
+        </button>
+        <p className="forgotPW">비밀번호를 잊으셨나요?</p>
+      </form>
+    </div>
+  );
+};
 
-export default LoginShin
+export default Login;
